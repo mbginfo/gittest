741033131852cd6faf5323d720c016b3d5bd600f
mbginfo <mbg.information@gmail.com>
2017-05-11 16:56:49 +0200

    MVC Project

---
+++ MVC/InputScreenMVC/InputScreenMVC/Controllers/CustomerController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InputScreenMVC.Models;
+
+namespace InputScreenMVC.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        private List<Customer> Customers = new List<Customer>();
+
+        public CustomerController()
+        {
+            Customer obj1 = new Customer();
+            obj1.Id = 11;
+            obj1.CustomerCode = "101";
+            obj1.Amount = 11.11;
+
+            Customers.Add(obj1);
+
+            obj1 = new Customer();
+            obj1.Id = 22;
+            obj1.CustomerCode = "202";
+            obj1.Amount = 22.22;
+
+            Customers.Add(obj1);
+
+        }
+
+        //public CustomerController()
+
+        //{
+        //    Customer obj1 = new Customer();
+        //    obj1.Id = 11;
+        //    obj1.CustomerCode = "101";
+        //    obj1.Amount = 11.11;
+
+        //    Customers.Add(obj1);
+
+        //    Customer obj2 = new Customer();
+        //    obj2.Id = 22;
+        //    obj2.CustomerCode = "202";
+        //    obj2.Amount = 22.22;
+
+        //    Customers.Add(obj2);
+
+        //}
+
+    public ActionResult FillCustomer()
+        {
+            return View();
+        }
+        //[HttpPost]
+        public ActionResult DisplayCustomer(int Id)
+        {
+            Customer objCustomer = Customers[Id];
+           return View("DisplayCustomer", objCustomer);
+        }
+    }
+}
---
+++ MVC/InputScreenMVC/MVCUnitTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InputScreenMVC.Controllers;
+using System.Web.Mvc;
+
+
+namespace MVCUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DisplayCustomer()
+        {
+            CustomerController obj = new CustomerController();
+            var varresult = obj.DisplayCustomer();
+            Assert.AreEqual("DisplayCustomer", varresult);
+        }
+    }
+}
